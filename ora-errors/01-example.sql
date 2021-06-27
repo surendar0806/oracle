dad456f3c52f9bb8f969bd0a5d8c2fd48e895fcf
surendar0806 <50395000+surendar0806@users.noreply.github.com>
2021-06-27 13:46:24 +0530

    Update and rename trouble shoot to ora-errors/01-example.sql

--- ora-errors/01-example.sql
+++ ora-errors/01-example.sql
@@ -1,6 +1,11 @@
+/* Basic errors
 Your table is already locked by some query. 
 ERROR at line 1: ORA-00054: resource busy and acquire with NOWAIT specified or timeout expired
-=====================================================
+*/
+
+-- ----------------------------------------------------------------------------------------
+-- To get the details of the locked objects
+-- --------------------------------------------------------------------------------------
 SELECT O.OBJECT_NAME, S.SID, S.SERIAL#, P.SPID, S.PROGRAM,S.USERNAME,
 S.MACHINE,S.PORT , S.LOGON_TIME,SQ.SQL_FULLTEXT 
 FROM V$LOCKED_OBJECT L, DBA_OBJECTS O, V$SESSION S, 
@@ -11,10 +16,3 @@ AND S.SQL_ADDRESS = SQ.ADDRESS;
 
 ALTER SYSTEM KILL SESSION '25,54324' IMMEDIATE;
 
-begin
-    rdsadmin.rdsadmin_util.kill(
-        sid    => &sid, 
-        serial => &serial_number,
-        method => 'IMMEDIATE');
-end;
-/
