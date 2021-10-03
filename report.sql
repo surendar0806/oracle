ea91790d7d42d7d27acf7caa0eaa071100b94f04
surendar0806 <50395000+surendar0806@users.noreply.github.com>
2021-10-03 12:41:24 +0530

    Update report.sql

--- report.sql
+++ report.sql
@@ -5,3 +5,11 @@ cd /opt/oracle.ExaWatcher
 ./GetExaWatcherResults.sh --from 09/12/2021_15:00:00 --to 09/12/2021_18:00:00 --resultdir /tmp/exawatcher_12092021
 
 */
+
+
+--AWR report scripts
+@$ORACLE_HOME/rdbms/admin/awrrpt.sql
+@$ORACLE_HOME/rdbms/admin/awrrpti.sql
+
+--ASH Report scripts
+$ORACLE_HOME/rdbms/admin/ashrpt.sql
