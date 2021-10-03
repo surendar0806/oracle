ebd1e52f62f90012b9b03938db4a98eaf2c8ff8e
surendar0806 <50395000+surendar0806@users.noreply.github.com>
2021-10-03 12:32:43 +0530

    Create report.sql

---
+++ report.sql
@@ -0,0 +1,7 @@
+/*   
+To generate  Oracle Exawatcher
+-----------------------------------------
+cd /opt/oracle.ExaWatcher
+./GetExaWatcherResults.sh --from 09/12/2021_15:00:00 --to 09/12/2021_18:00:00 --resultdir /tmp/exawatcher_12092021
+
+*/
