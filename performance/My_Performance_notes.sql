9aba83dbdac7042ba05aa6a75348a8ccf4a246d0
surendar0806 <50395000+surendar0806@users.noreply.github.com>
2022-08-13 21:39:56 +0530

    Update My_Performance_notes.sql

--- performance/My_Performance_notes.sql
+++ performance/My_Performance_notes.sql
@@ -7,3 +7,16 @@ fetches,executions,optimizer cost , sorts
 select sql_id,plan_hash_value,fetches,executions,sorts,optimizer_cost from gv$sql where sql_id='&SQL_ID';
 
 select inst_id,username,status,count(*) from gv$session where username is not null; 
+
+/* #General 
+Not every query running slow is issues of database 
+Need to check if the query runs single or as part of loop
+Understand the bussiness requirement and then start tuning. 
+Ask from when we start facing issues
+*/
+
+/*  data volume affecting the #query_plan
+consider a scenario from company table where the dept='it' fetches only 10% of data 
+whereas  dept='delivery' fetches rows more than 30%. 
+here the plan changes pased on the id passed. so it is important to understand the query purpose 
+*/
