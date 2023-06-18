16c60275acbcf679f595baa7e39a3df18c2180b6
surendar0806 <surendar080694@outlook.com>
2023-06-18 10:41:01 +0530

    Commit on 18-Jun-23

---
+++ performance/00-My_Perf_notes.sql
@@ -0,0 +1,27 @@
+/*
+We need to check the below column in the gv$sql. Need to analyze how often the transcation is successful
+there may be some case where where query is being callled multiple times
+fetches,executions,optimizer cost , sorts 
+*/
+
+select sql_id,plan_hash_value,fetches,executions,sorts,optimizer_cost from gv$sql where sql_id='&SQL_ID';
+
+select inst_id,username,status,count(*) from gv$session where username is not null; 
+
+/* #General 
+Not every query running slow is issues of database 
+Need to check if the query runs single or as part of loop
+Understand the bussiness requirement and then start tuning. 
+Ask from when we start facing issues
+*/
+
+/*  data volume affecting the #query_plan
+consider a scenario from company table where the dept='it' fetches only 10% of data 
+whereas  dept='delivery' fetches rows more than 30%. 
+here the plan changes pased on the id passed. so it is important to understand the query purpose 
+*/
+
+/* Index 
+sometimes we create a index for one query run becomes a overhead of the other query. 
+As temp workaround we can compare the plan from the awr and current execution make index invisible 
+*/
